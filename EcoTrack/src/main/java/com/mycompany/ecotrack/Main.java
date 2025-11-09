61f087427b65045725b7f24b34df2b343d076540
Josue Martinez <102492225+JosueMT505@users.noreply.github.com>
2025-11-08 21:58:32 -0500

    Carpeta de proyecto Ecotrack para implementar las clases

---
+++ EcoTrack/src/main/java/com/mycompany/ecotrack/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecotrack;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
