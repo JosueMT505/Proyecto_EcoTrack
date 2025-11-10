d918d9281d2e97b1fa9507c3be49b15053a3c0a7
Cristina Plaza <crismina@espol.edu.ec>
2025-11-09 23:10:13 -0500

    Update ComparadorPrioridad.java

--- EcoTrack/src/main/java/com/mycompany/ecotrack/ComparadorPrioridad.java
+++ EcoTrack/src/main/java/com/mycompany/ecotrack/ComparadorPrioridad.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Grupo_08.comparadores;
 
-import Grupo_08.modelo.Residuo;
+package com.mycompany.ecotrack;
+
 import java.util.Comparator;
 
 /**
  *
- * @author Usuario
+ * @author Grupo 8
  */
 public class ComparadorPrioridad implements Comparator<Residuo> {
     @Override
     public int compare(Residuo r1, Residuo r2) {
-        // Orden descendente: r2.prioridad - r1.prioridad
         return Integer.compare(r2.getNivelPrioridadAmbiental(), r1.getNivelPrioridadAmbiental());
     }
 }
