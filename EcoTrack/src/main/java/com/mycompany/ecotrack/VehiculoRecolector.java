cf152c8067a3ca3011f706c8ac83d8651f9fb7c9
Cristina Plaza <crismina@espol.edu.ec>
2026-01-15 20:45:37 -0500

    Update and rename

--- EcoTrack/src/main/java/com/mycompany/ecotrack/VehiculoRecolector.java
+++ EcoTrack/src/main/java/com/mycompany/ecotrack/VehiculoRecolector.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
  *
  * @author Grupo 8
  */
-public class Vehiculo implements Serializable {
+public class VehiculoRecolector implements Serializable {
     private String id;
     private double capacidadMaxima; 
     private double volumenActual;
@@ -15,7 +15,7 @@ public class Vehiculo implements Serializable {
     private boolean enRuta;
     private double nivelLlenado; 
 
-    public Vehiculo(String id, double capacidadMaxima, String zonaAsignadaId) {
+    public VehiculoRecolector(String id, double capacidadMaxima, String zonaAsignadaId) {
         this.id = id;
         this.capacidadMaxima = capacidadMaxima;
         this.zonaAsignadaId = zonaAsignadaId;
@@ -25,21 +25,35 @@ public class Vehiculo implements Serializable {
     }
 
 
-    public double getNivelLlenado() {
-        return volumenActual / capacidadMaxima; 
-    }
     
-    public void cargarResiduos(double volumen) {
-        this.volumenActual += volumen;
+public void cargarResiduos(double volumen) {
+        if (this.volumenActual + volumen > this.capacidadMaxima) {
+            System.out.println("Error: El vehículo " + id + " se ha llenado al máximo.");
+            this.volumenActual = this.capacidadMaxima;
+        } else {
+            this.volumenActual += volumen;
+        }
         this.nivelLlenado = getNivelLlenado();
     }
+    public void vaciarVehiculo() {
+        this.volumenActual = 0.0;
+        this.nivelLlenado = 0.0;
+        this.enRuta = false; 
+    }
+
+
     
-
-
+    public double getNivelLlenado() {return volumenActual / capacidadMaxima;}
     public String getId() { return id; }
     public double getCapacidadMaxima() { return capacidadMaxima; }
     public double getVolumenActual() { return volumenActual; }
     public String getZonaAsignadaId() { return zonaAsignadaId; }
     public boolean isEnRuta() { return enRuta; }
     public void setEnRuta(boolean enRuta) { this.enRuta = enRuta; }
+}
+
+@Override
+    public String toString() {
+        return "Vehículo " + id + " [Zona: " + zonaAsignadaId + ", Llenado: " + String.format("%.2f", nivelLlenado * 100) + "%]";
+    }
 }
